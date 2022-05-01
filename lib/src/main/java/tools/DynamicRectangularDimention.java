00d796d56711cf8ffa51d42bb29133b16a6a6535
Asror <asrorklichev@yahoo.com>
2022-04-30 22:50:10 -0400

    Delete unneccesary class

--- lib/src/main/java/tools/DynamicRectangularDimention.java
+++
@@ -1,9 +0,0 @@
-package tools;
-
-public class DynamicRectangularDimention extends RectangularDimention{
-
-	public DynamicRectangularDimention(double prefWidth, double prefHeight, double minWidth, double minHeight) {
-		super(prefWidth, prefHeight, Integer.MAX_VALUE, Integer.MAX_VALUE, minWidth, minHeight);
-	}
-	
-}
